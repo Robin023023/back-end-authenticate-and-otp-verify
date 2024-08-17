970adac5c6c80fc0d0fddf42240ce677736fad05
Robin Hossain <023023robin@gmail.com>
2024-08-17 12:59:42 +0600

    first commit

--- index.js
+++ index.js
@@ -2,6 +2,4 @@ const app=require("./app")
 const  config=require("./config/config")
 const port=config.app.port;
 
-app.listen(port,()=>{
-    console.log(`Server is running at http://localhost:${port}`)
-})
+app.listen(port)
